8e0fba36cdd074b9d2baeebbb8129b29fd352ca9
linuskang <linus@kang.id.au>
2026-04-15 14:08:57 +1000

    Fix login screen

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -15,7 +15,7 @@ export default function Login() {
     // States
     const [error, setError] = useState<string | null>(null);
     const [isPending, setIsPending] = useState(false);
-    const [appInfo, setAppInfo] = useState<{ version: string; build: string }>({ version: "dev", build: "dev build" });
+    const [appInfo, setAppInfo] = useState<{ version: string }>({ version: "dev" });
 
     // Get version metadata
     useEffect(() => {
@@ -23,15 +23,15 @@ export default function Login() {
             try {
                 const response = await fetch("/api");
                 const data = await response.json();
-                setAppInfo({ version: data.version, build: data.build });
-            } catch (e) {
-                console.error("Failed to fetch app info");
+                setAppInfo({ version: data.version });
+            } catch (e: any) {
+                console.error(e.message);
             }
         };
         fetchAppInfo();
     }, []);
 
-    // GitHub Login Handler
+    // GitHub Login
     const handleGithubLogin = async () => {
         setError(null);
         setIsPending(true);
@@ -69,10 +69,13 @@ export default function Login() {
                             disabled={isPending}
                             className="w-full h-11 cursor-pointer flex items-center justify-center"
                         >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                            </svg>
                             {isPending ? "Redirecting..." : "Continue with GitHub"}
                         </Button>
 
-                        <div className="space-y-4">
+                        <div>
                             <p className="text-xs text-muted-foreground text-center">
                                 By signing in, you agree to abide by our{" "}
                                 <a href="/terms" className="underline">Terms</a>,{" "}
@@ -80,20 +83,17 @@ export default function Login() {
                                 <a href="/community-guidelines" className="underline">Community Guidelines</a>.
                             </p>
 
-                            {/* <p className="text-xs text-muted-foreground text-center">
-                                Don't have an account?{" "}
-                                <a href="/onboarding" className="underline">
-                                    Sign up
-                                </a>
-                            </p> */}
-
-                            <p className="mt-8 text-xs text-muted-foreground text-center">
-                                {appInfo.version} ({appInfo.build})
+                            <p className="mt-5 text-xs text-muted-foreground text-center">
+                                v{appInfo.version}
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <footer className="fixed bottom-4 left-0 right-0 text-center text-xs text-muted-foreground">
+                (c) Linus Kang 2026. All Rights Reserved.
+            </footer>
         </div>
     );
 }
